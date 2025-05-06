e8b0dae13c9a3cc68d488a5b8195ae971dc2ce28
Araw <markistan123@gmail.com>
2025-05-07 06:19:11 +0800

    asdadas
    asdadas

---
+++ chances 2.0/Assets/Scenes/Updated Scenes/ExitAPP.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitAPP : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+
+    }
+}
