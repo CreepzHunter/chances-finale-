fa2e3c6944ad1001ee3078885b8a1254f250db44
Sandwick <lloyd.clarete@usjr.edu.ph>
2025-05-07 10:12:21 +0800

    Hotkey for Bag/Inventory

--- chances 2.0/Assets/Scenes/Updated Scenes/eventscript/map.cs
+++ chances 2.0/Assets/Scenes/Updated Scenes/eventscript/map.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class map : MonoBehaviour
 {
     public GameObject Map;
+    public GameObject Bag;
 
 
     void Update()
     {
-         {
+        {
             if (Input.GetKeyDown(KeyCode.M) && Map != null)
             {
                 Map.SetActive(!Map.activeSelf);
+            }
+            if ((Input.GetKeyDown(KeyCode.N) || Input.GetKeyDown(KeyCode.Tab)) && Bag != null)
+            {
+                Bag.SetActive(!Bag.activeSelf);
             }
         }
     }
